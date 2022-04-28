d95e09a5cc63f9270aca4593202cc47edd4d5050
Clément Peloux <cpeloux@aix-mac-89.local>
2022-04-28 16:27:29 +0200

    front

--- src/App.js
+++ src/App.js
@@ -3,25 +3,41 @@ import './App.css';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const cities = [
-  {label : 'Marseille', coordinates: '43.296482, 5.36978'}
-];
+
+function uniqByKeepFirst(a, key) {
+  let seen = new Set();
+  return a.filter(item => {
+      let k = key(item);
+      return seen.has(k) ? false : seen.add(k);
+  });
+}
 
 function App() {
+  let json = require('./cities.json');
+  json = uniqByKeepFirst(json, it => it.label);
+  json = json.map((cityHash) => {
+    return {label : cityHash.label, coordinates : cityHash.coordinates};
+  });
+  json = json.filter((cityHash) => {
+    return cityHash.label != null && cityHash.coordinates != null;
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" style={{height: 200 }} />
         <h1>
           Hackaton
         </h1>
-        <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          options={cities}
-          sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Cities" />}
-        />
+        <div className="autocomplete">
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={json}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField className="textfield" {...params} label="Ville"/>}
+          />
+        </div>
       </header>
     </div>
   );
