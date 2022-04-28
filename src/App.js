d4a6d006105e50087c47c4ff1c5a20bf210cdbbe
mbrenguier <mbrenguier@voyageprive.com>
2022-04-28 14:24:23 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+const cities = [
+  {label : 'Marseille', coordinates: '43.296482, 5.36978'}
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Hackaton
+        </h1>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={cities}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Cities" />}
+        />
       </header>
     </div>
   );
